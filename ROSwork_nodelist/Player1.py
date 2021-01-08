17386be5c7815d227a9dfa905cede2d468d1db9f
mekameka2020 <71114217+mekameka2020@users.noreply.github.com>
2021-01-08 23:40:49 +0900

    Update Player1.py

--- ROSwork_nodelist/Player1.py
+++ ROSwork_nodelist/Player1.py
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 /*
- * Copyright (C) 2020 Ryuichi Ueda.  All rights reserved.
+ * Copyright (C) 2021 Kawano Hidetoshi and Ryuichi Ueda.  All rights reserved.
  
  This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
