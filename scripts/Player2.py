cbb5c94b5b68038c9aa62a083e68beccfa97e021
mekameka2020 <71114217+mekameka2020@users.noreply.github.com>
2021-01-10 14:14:29 +0900

    Update Player2.py

--- scripts/Player2.py
+++ scripts/Player2.py
@@ -1,7 +1,7 @@
-// SPDX-License-Identifier: GPL-3.0-only
-/*
- * Copyright (C) 2021 Kawano Hidetoshi and Ryuichi Ueda.  All rights reserved.
- 
+# SPDX-License-Identifier: GPL-3.0-only
+
+ # Copyright (C) 2021 Kawano Hidetoshi and Ryuichi Ueda.  All rights reserved.
+ '''
  This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -12,7 +12,7 @@
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ '''
 
 #!/usr/bin/env python3
 
